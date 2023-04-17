4acb302093eeb888bbdcfa85694cd8ad0d604fbb
mpdnes <mpdnes@rit.edu>
2023-04-17 15:31:45 -0400

    added word cloud
    Word cloud added to this branch.

--- TAPv0_1_1/report_these_words.py
+++ TAPv0_1_1/report_these_words.py
@@ -11,6 +11,15 @@ def report_these_words( dict_of_all_words ):
     # Python 3 has a new method.
     # AND, when working with keys, you must use both the key and the value in a for loop.
     # print( dict_of_all_words )
+    from wordcloud import WordCloud
+    import matplotlib.pyplot as plt
+    from PIL import Image
+
+    wc = WordCloud(background_color="white", width=1000,height=1000, max_words=len(dict_of_all_words),
+                   relative_scaling=0.5,normalize_plurals=False).generate_from_frequencies(dict_of_all_words)
+
     for word,freq in dict_of_all_words.items():
         print( "word = ", word, '\t\t', end='' )
         print( "freq = ", freq )
+
+        return wc
